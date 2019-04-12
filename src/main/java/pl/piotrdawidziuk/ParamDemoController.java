5e21c20d6297857a72602b720dc016c6cff1dd22
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-12 10:35:09 +0200

    optional params

--- src/main/java/pl/piotrdawidziuk/ParamDemoController.java
+++ src/main/java/pl/piotrdawidziuk/ParamDemoController.java
@@ -1,5 +1,31 @@
 package pl.piotrdawidziuk;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class ParamDemoController {
+	
+	//https://www.baeldung.com/spring-request-param
+	
+//	@GetMapping("/api/foos")
+//	@ResponseBody
+//	public String getFoos(@RequestParam String id) {
+//	    return "ID: " + id;
+//	}
+	
+	@GetMapping("/api/foos")
+	@ResponseBody
+	public String getFoos(@RequestParam(required = false) String id) { 
+	    
+		String idParam = id;
+		if(idParam == null) {
+			idParam = "Cannot find Id";
+		}
+		
+		return "ID: " + idParam;
+	}
 
 }
